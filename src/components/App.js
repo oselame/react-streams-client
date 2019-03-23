e096396123e3331d08c84d891ae9f4498d937271
Adriano Oselame <adrianooselame@gmail.com>
2019-03-22 23:08:26 -0300

    Rest-Based react apps

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
-import { BrowserRouter, Route } from 'react-router-dom';
-
+import { Router, Route } from 'react-router-dom';
+import history from '../history';
 
 import Header from './Header';
 import StreamCreate from './streams/StreamCreate';
@@ -14,16 +14,16 @@ class App extends Component {
   render() {
     return (
       <div className="ui container">
-        <BrowserRouter>
+        <Router history={history}>
           <div>
             <Header />  
             <Route path="/" exact component={StreamList} />
             <Route path="/streams/new" exact component={StreamCreate} />
-            <Route path="/streams/edit" exact component={StreamEdit} />
+            <Route path="/streams/edit/:id" exact component={StreamEdit} />
             <Route path="/streams/delete" exact component={StreamDelete} />
             <Route path="/streams/show" exact component={StreamShow} />
           </div>
-        </BrowserRouter>
+        </Router>
       </div>
     );
   }
